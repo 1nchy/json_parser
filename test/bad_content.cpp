5026fc30c7c8aeb42c9ed2019ecf37eb534ceea8
1nchy <wangyinqi23@mails.ucas.ac.cn>
2025-03-31 10:12:37 +0800

    refact(test): rename exception_handle to bad_content

--- test/bad_content.cpp
+++ test/bad_content.cpp
@@ -19,6 +19,7 @@ int main(void) {
     icy_assert(about_bad_content(exception::RIGHT_CURLY_EXPECTED, "{\"first\":1st"));
     icy_assert(about_bad_content(exception::RIGHT_SQUARE_EXPECTED, "["));
     icy_assert(about_bad_content(exception::RIGHT_SQUARE_EXPECTED, "[1st"));
+    icy_assert(about_bad_content(exception::RIGHT_SQUARE_EXPECTED, "[{},"));
     icy_assert(about_bad_content(exception::COLON_EXPECTED, "{\"\"}"));
     icy_assert(about_bad_content(exception::STRING_KEY_EXPECTED, "{one:1}"));
     icy_assert(about_bad_content(exception::STRING_KEY_EXPECTED, "{:1}"));
@@ -26,6 +27,9 @@ int main(void) {
     icy_assert(about_bad_content(exception::VALUE_EXPECTED, "]"));
     icy_assert(about_bad_content(exception::VALUE_EXPECTED, ","));
     icy_assert(about_bad_content(exception::VALUE_EXPECTED, "truth"));
+    icy_assert(about_bad_content(exception::VALUE_EXPECTED, "{\"first\":}"));
+    icy_assert(about_bad_content(exception::VALUE_EXPECTED, "{\"\":[}]"));
+    icy_assert(about_bad_content(exception::VALUE_EXPECTED, "{\"\":[{},}]"));
     icy_assert(about_bad_content(exception::END_OF_STRING_EXPECTED, "\"1nchy"));
     icy_assert(about_bad_content(exception::END_OF_STRING_EXPECTED, "\"1n\nchy\""));
     icy_assert(about_bad_content(exception::END_OF_NUMBER_EXPECTED, "1."));
